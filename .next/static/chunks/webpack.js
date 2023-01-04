1922a2fbac5d7151188499f0afecc89cc3416970
marianfrancisco <mfrancisco9@my.bcit.ca>
2023-01-03 18:19:07 -0800

    added header and footer

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bae4b039b7099636"; }
+/******/ 		__webpack_require__.h = function() { return "9a5e9bd3a6d3f6e9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
